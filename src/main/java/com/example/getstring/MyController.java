1e34da390260b78fdcd942448298c35ad1270a14
hymasri <hymasribonam3@gmail.com>
2023-04-06 12:57:20 +0000

    solution

---
+++ src/main/java/com/example/getstring/MyController.java
@@ -0,0 +1,19 @@
+// package com.example.getstring;
+
+
+// public class MyController {
+
+// }
+package com.example.getstring;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+public class MyController{
+    @GetMapping("/")
+    public String getString(){
+        return "Welcome to Spring Boot";
+    }
+
+}
